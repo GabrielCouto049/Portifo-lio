ec1180931964988704281665ca47e1c2b959ddd8
Gabriel Couto <150935713+GabrielCouto049@users.noreply.github.com>
2026-07-09 18:55:24 -0300

    Add animations and enhance styling for components
    - Introduced new animations using Lenis and Typed.js for smooth scrolling and typing effects.
    - Updated styles for sections and skill cards for better visual consistency.
    - Added new dependencies in package.json: lenis and typed.js.
    - Refactored multiple components to utilize new styles and animations.

---
+++ src/animations/typing.js
@@ -0,0 +1,11 @@
+  import Typed from "typed.js";
+
+  new Typed("#typed", {
+    strings: [
+      "Desenvolvedor Front-end",
+      "Entusiasta de TypeScript",
+    ],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true,
+  });
